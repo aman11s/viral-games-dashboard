dbf7fcaae593955d72e76f0e89178f0c01c07755
Aman Singh <reachout.amansingh@gmail.com>
2022-12-01 19:26:08 +0530

    feat : add auth context

---
+++ src/context/auth-context.js
@@ -0,0 +1,17 @@
+import { createContext, useContext, useState } from "react";
+
+const authContext = createContext(null);
+
+const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <authContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+const useAuth = () => useContext(authContext);
+
+export { AuthProvider, useAuth };
