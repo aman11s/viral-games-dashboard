fca333b8d79147a48719bc3b4e785ecf51265f4b
Aman Singh <reachout.amansingh@gmail.com>
2022-12-01 19:40:58 +0530

    feat : login handler

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuth } from "../../context";
 import "./Login.css";
 
+const testCredentials = {
+  username: "aman11s",
+  password: "amansingh",
+};
+
+const initialFormData = {
+  username: "",
+  password: "",
+};
 export const Login = () => {
+  const { setIsLoggedIn } = useAuth();
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const { username, password } = formData;
+
+    if (username === "aman11s" && password === "amansingh") {
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <div className="login-page">
       <h1>Login</h1>
-      <form className="login-form my-2 p-4">
+      <form onSubmit={submitHandler} className="login-form my-2 p-4">
         <label className="my-2">
           <div className="mb-1">Username : </div>
-          <input type="text" />
+          <input
+            required
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, username: e.target.value }))
+            }
+            value={formData.username}
+            type="text"
+          />
         </label>
         <label className="my-2">
           <div className="mb-1">Password : </div>
-          <input type="password" />
+          <input
+            required
+            onChange={(e) =>
+              setFormData((prev) => ({ ...prev, password: e.target.value }))
+            }
+            value={formData.password}
+            type="password"
+          />
         </label>
-        <button type="submit" className="btn primary-btn mx-auto">
+        <button
+          onClick={submitHandler}
+          type="submit"
+          className="btn primary-btn mx-auto"
+        >
+          Login
+        </button>
+        <button
+          onClick={() => setFormData(testCredentials)}
+          type="button"
+          className="btn primary-btn mx-auto my-2"
+        >
           Use test credentials
         </button>
       </form>
