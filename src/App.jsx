8ef13e68dd98791835779b063ff5d83c9b00efeb
Aman Singh <reachout.amansingh@gmail.com>
2022-12-02 18:14:14 +0530

    feat : user logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { RequiresAuth } from "./components";
+import { useAuth } from "./context";
 import { Dashboard, Login, SingleGame } from "./pages";
 
 function App() {
+  const { isLoggedIn, isUserLoggedIn } = useAuth();
+
   return (
     <div className="App">
+      {isLoggedIn && (
+        <button
+          onClick={() => isUserLoggedIn(false)}
+          className="btn logout-btn"
+        >
+          Logout
+        </button>
+      )}
+
       <Routes>
         {/* Public Route */}
         <Route path="/login" element={<Login />} />
